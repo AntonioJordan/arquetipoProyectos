82f4b53f2662c1cc630766c8837c076233ba7bc5
AntonioJordan <tonijordanrodriguez6@gmail.com>
2024-02-15 02:24:07 +0100

    Subida proyecto.

---
+++ artefactoBase/src/main/java/com/base/artefactoBase/ArtefactoBaseApplication.java
@@ -0,0 +1,13 @@
+package com.base.artefactoBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtefactoBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtefactoBaseApplication.class, args);
+	}
+
+}
